4b6dfa96a439e1e0484a447f7f00eb77c73524f9
wanuch <wanuch.int@gmail.com>
2022-12-05 00:21:41 +0700

    optimize img

--- components/event-detail/event-logistics.tsx
+++ components/event-detail/event-logistics.tsx
@@ -2,6 +2,7 @@ import AddressIcon from '../icons/address-icon';
 import DateIcon from '../icons/date-icon';
 import LogisticsItem from './logistics-item';
 import classes from './event-logistics.module.css';
+import Image from "next/image";
 
 export default function EventLogistics(props: any) {
   const { date, address, image, imageAlt } = props;
@@ -16,7 +17,7 @@ export default function EventLogistics(props: any) {
   return (
     <section className={classes.logistics}>
       <div className={classes.image}>
-        <img src={`/${image}`} alt={imageAlt} />
+        <Image src={`/${image}`} alt={imageAlt} width={400} height={400} />
       </div>
       <ul className={classes.list}>
         <LogisticsItem icon={DateIcon}>
--- components/events/event-item.tsx
+++ components/events/event-item.tsx
@@ -3,6 +3,7 @@ import classes from "./event-item.module.css"
 import AddressIcon from "../icons/address-icon";
 import ArrowRightIcon from "../icons/arrow-right-icon";
 import DateIcon from "../icons/date-icon";
+import Image from "next/image";
 
 
 export default function EventItem(props: any) {
@@ -20,7 +21,7 @@ export default function EventItem(props: any) {
     const explorLink = `/events/${id}`;
 
     return (<li className={classes.item}>
-        <img src={'/' + image} alt={title} />
+        <Image src={'/' + image} alt={title} width={250} height={160} />
         <div className={classes.content}>
             <div className={classes.summary}>
                 <h2>{title}</h2>
